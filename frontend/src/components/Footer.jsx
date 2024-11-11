81f4335f91ea124215473428ec13b004f24753ed
Yianni Ziouras <jmziouras@gmail.com>
2024-11-10 21:37:13 -0800

    made footer images smaller

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="w-full mt-16">
       <div className="container mx-auto py-4 flex justify-center items-end">
         <div className="flex justify-center items-center space-x-4">
-          <div className="h-16 w-16 relative">
+          <div className="h-8 w-8 relative">
             <a
               href="https://www.instagram.com/clareejordanart/"
               target="_blank"
@@ -12,7 +12,7 @@ const Footer = () => {
               <img src="/icons/instagram.svg" alt="Instagram logo" />
             </a>
           </div>
-          <div className="h-16 w-16 relative">
+          <div className="h-8 w-8 relative">
             <a
               href="https://www.linkedin.com/in/clareejordan"
               target="_blank"
