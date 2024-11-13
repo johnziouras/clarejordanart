aabc7a6486f9c5be08d30c10ede7ab2407bded79
Yianni Ziouras <jmziouras@gmail.com>
2024-11-12 21:36:15 -0800

    okay for real this time

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,6 @@ connectDB();
 
 const app = express();
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
