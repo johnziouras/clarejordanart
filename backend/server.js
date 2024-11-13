48c6f262b86f6b6e36eb4fe6b55841b8063a31a1
Yianni Ziouras <jmziouras@gmail.com>
2024-11-13 10:39:47 -0800

    added title to index.html

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
 app.use("/api/artwork", require("./routes/artworkRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../", "frontend", "build", "index.html")
+    );
+  });
+}
 
 app.use(errorHandler);
 
