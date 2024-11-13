637a46c6518ecd17c2b63ce0a2f499a4ec79d275
Yianni Ziouras <jmziouras@gmail.com>
2024-11-12 21:14:36 -0800

    updated server to serve build files

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const { connectDB } = require("./config/db");
+const path = require("path");
+
 const port = process.env.PORT;
 
 connectDB();
 
 const app = express();
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,3 +23,5 @@ app.use("/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`));
+
+app.use(express.static(path.join(__dirname, "frontend/build")));
