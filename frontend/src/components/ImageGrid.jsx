7d8f489bc9bbd6e639afc4c250ec0ac4920d3ad3
Yianni Ziouras <jmziouras@gmail.com>
2025-08-20 12:04:04 -0700

    added dynamic column resizing based on screen width

--- frontend/src/components/ImageGrid.jsx
+++ frontend/src/components/ImageGrid.jsx
@@ -1,11 +1,26 @@
 import "photoswipe/dist/photoswipe.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { MoonLoader } from "react-spinners";
 import { getArtwork, reset } from "../features/artwork/artworkSlice";
 import CustomImage from "./CustomImage";
 
 const ImageGrid = ({ type }) => {
+  const [numCols, setNumCols] = useState(3);
+
+  // TODO: remove magic numbers, create constants file
+  useEffect(() => {
+    const updateCols = () => {
+      if (window.innerWidth < 640) setNumCols(2);
+      else if (window.innerWidth < 1024) setNumCols(3);
+      else setNumCols(4);
+    };
+
+    updateCols();
+    window.addEventListener("resize", updateCols);
+    return () => window.removeEventListener("resize", updateCols);
+  }, []);
+
   const dispatch = useDispatch();
   const { artwork, isLoading, isSuccess } = useSelector(
     (state) => state.artwork
@@ -27,17 +42,19 @@ const ImageGrid = ({ type }) => {
     );
   }
 
-  const columns = [[], [], []];
+  const columns = Array.from({ length: numCols }, () => []);
   if (isSuccess) {
     artwork.forEach((art, index) => {
-      columns[index % 3].push(<CustomImage artworkObj={art} />);
+      columns[index % numCols].push(<CustomImage artworkObj={art} />);
     });
   }
 
   return (
-    <div className="w-full grid grid-cols-3 grid-rows-1 gap-4 py-8 px-8 sm:px-16">
+    <div className="w-full flex gap-8 py-8 sm:px-8">
       {columns.map((column, colIndex) => (
-        <div key={colIndex}>{column}</div>
+        <div key={colIndex} className="flex-1 flex flex-col gap-4">
+          {column}
+        </div>
       ))}
     </div>
   );
